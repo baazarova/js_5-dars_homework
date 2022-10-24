3fbcd64f75254cf022675117151d146c876b27f2
baazarova <iskandarovaogiloy@gmail.com>
2022-10-24 09:56:05 +0500

    5-dars

---
+++ index.js
@@ -0,0 +1,42 @@
+//SHOKOLAD NARXI CHIQARILADIGAN DASTUR:
+
+// let usd = 5;
+// let weight = 50;
+
+// for(i = 1; i <=50; i++){
+//     console.log(i + 'kg' + usd * i + '$');
+// }
+
+
+//CALCULATOR:
+
+// let a = Number(prompt('son kiriting'))
+// let calc = prompt('arifmetik amal kiriting')
+// let b = Number(prompt('son kiritng'))
+
+// switch(1){
+//     case 1:
+//       if(calc == '+'){
+//         console.log(a + b);
+//       } 
+//       else if(calc == '-'){
+//         console.log(a - b);
+//       } 
+//       else if(calc == '*'){
+//         console.log(a * b);
+//       }
+//       else if(calc == '/'){
+//         console.log(a / b);
+//       }
+    
+//     default:
+//         "To'g'ri son kiriting!"
+// }
+
+
+
+//K VA N SONLAR DASTURI:
+
+// let k = Number(prompt('1-son kiriting'));
+// let n = Number(prompt('2-son kiriting'))
+
